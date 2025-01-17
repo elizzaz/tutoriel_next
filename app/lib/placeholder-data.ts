0fedd659915c1e0fcdb209657d4d1baf0cd59d57
Elizzaz <elisamorillon07@gmail.com>
2025-01-17 11:10:02 +0100

    chapter 8 completed

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -3,8 +3,8 @@
 const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
-    name: 'User',
-    email: 'user@gmail.com',
+    name: 'Eliza',
+    email: 'eliza@gmail.com',
     password: 'password',
   },
 ];
